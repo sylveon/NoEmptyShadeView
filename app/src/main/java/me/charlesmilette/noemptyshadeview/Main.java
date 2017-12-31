7ab3f93b2738bdf63d7d87859abd77176841b206
Charles Milette <charles.milette@gmail.com>
2017-12-31 15:31:04 -0500

    Update code to work with Nougat (I think)

--- app/src/main/java/me/charlesmilette/noemptyshadeview/Main.java
+++ app/src/main/java/me/charlesmilette/noemptyshadeview/Main.java
@@ -1,15 +1,35 @@
 package me.charlesmilette.noemptyshadeview;
 
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-import de.robv.android.xposed.XC_MethodReplacement;
+import android.os.Build;
 
-public class Main implements IXposedHookLoadPackage {
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedHelpers;
+
+public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResources {
+    private static final String SystemUI = "com.android.systemui";
+
+    private static boolean isNougatOrBetter() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
+    }
+
+
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        if (loadPackageParam.packageName.equals("com.android.systemui")) {
-            findAndHookMethod("com.android.systemui.statusbar.phone.NotificationPanelView", loadPackageParam.classLoader, "updateEmptyShadeView", XC_MethodReplacement.DO_NOTHING);
+        if (loadPackageParam.packageName.equals(SystemUI) && !isNougatOrBetter()) {
+            // N solution probably works with MM and LL too, but I have no device to check, so
+            // just use a solution that I know works on those.
+            XposedHelpers.findAndHookMethod(SystemUI + ".statusbar.phone.NotificationPanelView", loadPackageParam.classLoader, "updateEmptyShadeView", XC_MethodReplacement.DO_NOTHING);
+        }
+    }
+
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam initPackageResourcesParam) throws Throwable {
+        if (initPackageResourcesParam.packageName.equals(SystemUI) && isNougatOrBetter()) {
+            initPackageResourcesParam.res.setReplacement(SystemUI, "string", "empty_shade_text", "");
         }
     }
 }
